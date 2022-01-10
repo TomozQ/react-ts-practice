c6c5f6acb1841717c0a08cb15b0974aecb897288
TomozQ <inf.4clover@gmail.com>
2022-01-10 21:23:27 +0900

    liblary with ts

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import './NewTodo.css'
 
 type NewTodoProps = {
@@ -6,6 +6,8 @@ type NewTodoProps = {
 }
 
 const NewTodo: React.FC<NewTodoProps> = props => {
+    // useEffect()
+
     const textInputRef = useRef<HTMLInputElement>(null)
 
     const todoSubmitHandler = (event: React.FormEvent) => {
